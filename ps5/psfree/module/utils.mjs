0e5af1bb0563daf770b699298de2ddb3ec0f9300
ericwot11 <149178480+ericwot11@users.noreply.github.com>
2025-02-07 13:25:48 +0100

    Update utils.mjs

--- ps5/psfree/module/utils.mjs
+++ ps5/psfree/module/utils.mjs
@@ -1,20 +1,3 @@
-/* Copyright (C) 2023-2024 anonymous
-
-This file is part of PSFree.
-
-PSFree is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-PSFree is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
-
 import { Int } from './int64.mjs';
 
 export class DieError extends Error {
@@ -24,66 +7,49 @@ export class DieError extends Error {
     }
 }
 
-export function die(msg='') {
+export function die(msg = '') {
+    logError(`Erreur critique : ${msg}`);
     throw new DieError(msg);
 }
 
-const console = document.getElementById('console');
-// export function debug_log(msg='') {
-//     console.append(msg + '\n');
-// }
-export const debug_log = print; 
-window.debug_log = debug_log;
+export const debug_log = (msg = '') => {
+    console.log(msg);  // Affichage dans la console
+    document.getElementById('console').innerText += msg + '\n';
+};
 
 export function clear_log() {
-    console.innerHTML = null;
+    document.getElementById('console').innerHTML = '';
 }
 
-export function str2array(str, length, offset) {
-    if (offset === undefined) {
-        offset = 0;
-    }
-    let a = new Array(length);
+export function str2array(str, length, offset = 0) {
+    let arr = new Array(length);
     for (let i = 0; i < length; i++) {
-        a[i] = str.charCodeAt(i + offset);
+        arr[i] = str.charCodeAt(i + offset);
     }
-    return a;
+    return arr;
 }
 
-// alignment must be 32 bits and is a power of 2
 export function align(a, alignment) {
-    if (!(a instanceof Int)) {
-        a = new Int(a);
-    }
+    if (!(a instanceof Int)) a = new Int(a);
     const mask = -alignment & 0xffffffff;
-    let type = a.constructor;
-    let low = a.low & mask;
-    return new type(low, a.high);
+    return new a.constructor(a.low & mask, a.high);
 }
 
-export async function send(url, buffer, file_name, onload=() => {}) {
-    const file = new File(
-        [buffer],
-        file_name,
-        {type:'application/octet-stream'}
-    );
-    const form = new FormData();
-    form.append('upload', file);
-
-    debug_log('send');
-    const response = await fetch(url, {method: 'POST', body: form});
-
-    if (!response.ok) {
-        throw Error(`Network response was not OK, status: ${response.status}`);
+export async function send(url, buffer, fileName, onload = () => {}) {
+    try {
+        const file = new File([buffer], fileName, { type: 'application/octet-stream' });
+        const form = new FormData();
+        form.append('upload', file);
+        
+        let response = await fetch(url, { method: 'POST', body: form });
+        if (!response.ok) throw new Error(`Échec de l’envoi du fichier : ${response.status}`);
+        onload();
+    } catch (error) {
+        logError(`Erreur d'envoi : ${error}`);
     }
-    onload();
 }
 
-// mostly used to yield to the GC. marking is concurrent but collection isn't
-//
-// yielding also lets the DOM update. which is useful since we use the DOM for
-// logging and we loop when waiting for a collection to occur
-export function sleep(ms=0) {
+export function sleep(ms = 0) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
@@ -91,7 +57,11 @@ export function hex(number) {
     return '0x' + number.toString(16);
 }
 
-// no "0x" prefix
 export function hex_np(number) {
     return number.toString(16);
+}
+
+function logError(message) {
+    console.error(message);
+    debug_log(`[Erreur] ${message}`);
 }
