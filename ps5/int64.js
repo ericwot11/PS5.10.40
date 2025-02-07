8ac0b95bf4f01591e9600324c02cd56e11909617
ericwot11 <149178480+ericwot11@users.noreply.github.com>
2025-02-07 13:10:17 +0100

    Update int64.js

--- ps5/int64.js
+++ ps5/int64.js
@@ -5,82 +5,36 @@ function int64(low, hi) {
     this.add32inplace = function (val) {
         let new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
         let new_hi = (this.hi >>> 0);
-
-        if (new_lo < this.low) {
-            new_hi++;
-        }
-
+        if (new_lo < this.low) new_hi++;
         this.hi = new_hi;
         this.low = new_lo;
     }
 
-    this.add32 = function (val) {
-        let new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
-        let new_hi = (this.hi >>> 0);
-
-        if (new_lo < this.low) {
-            new_hi++;
-        }
-
-        return new int64(new_lo, new_hi);
+    this.add64 = function (other) {
+        let result_lo = (this.low + other.low) >>> 0;
+        let carry = (result_lo < this.low) ? 1 : 0;
+        let result_hi = (this.hi + other.hi + carry) >>> 0;
+        return new int64(result_lo, result_hi);
     }
 
-    this.sub32 = function (val) {
-        let new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
-        let new_hi = (this.hi >>> 0);
-
-        if (new_lo > (this.low) & 0xFFFFFFFF) {
-            new_hi--;
-        }
-
-        return new int64(new_lo, new_hi);
-    }
-
-    this.sub32inplace = function (val) {
-        let new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
-        let new_hi = (this.hi >>> 0);
-
-        if (new_lo > (this.low) & 0xFFFFFFFF) {
-            new_hi--;
-        }
-
-        this.hi = new_hi;
-        this.low = new_lo;
-    }
-
-    this.and32 = function (val) {
-        let new_lo = this.low & val;
-        let new_hi = this.hi;
-        return new int64(new_lo, new_hi);
-    }
-
-    this.and64 = function (vallo, valhi) {
-        let new_lo = this.low & vallo;
-        let new_hi = this.hi & valhi;
+    this.xor64 = function (vallo, valhi) {
+        let new_lo = this.low ^ vallo;
+        let new_hi = this.hi ^ valhi;
         return new int64(new_lo, new_hi);
     }
 
     this.toString = function () {
-        let lo_str = (this.low >>> 0).toString(16);
+        let lo_str = (this.low >>> 0).toString(16).padStart(8, '0');
         let hi_str = (this.hi >>> 0).toString(16);
-
-        if (this.hi == 0)
-            return lo_str;
-        else
-            lo_str = zeroFill(lo_str, 8)
-
-        return hi_str + lo_str;
+        return hi_str === "0" ? lo_str : hi_str + lo_str;
     }
 
     return this;
 }
 
-function zeroFill(number, width) {
-    width -= number.toString().length;
+// Exemple d'utilisation :
+let address1 = new int64(0xdeadbeef, 0x00000001);
+let address2 = new int64(0x00000010, 0x00000000);
+let result = address1.add64(address2);
+console.log(result.toString());  // Affiche l'adresse finale
 
-    if (width > 0) {
-        return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
-    }
-
-    return number + ""; // always return a string
-}
