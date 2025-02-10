521cf9aeb12c6f67d04684b64baa77b1704b6527
ericwot11 <149178480+ericwot11@users.noreply.github.com>
2025-02-10 07:00:40 +0100

    Delete ps5 directory

--- ps5/rop.js
+++
@@ -1,6 +0,0 @@
-function executeROPChain() {
-    addLog("Exécution de la chaîne ROP pour contourner la sandbox...");
-    setTimeout(() => {
-        addLog("Chaîne ROP exécutée avec succès, sandbox contournée !", "success");
-    }, 2500);
-}
